129c956ddd5d202658a8620370c8d363d592facb
palascinochristian <palascinochristian@gmail.com>
2025-03-04 15:58:05 +0100

    feat: add Routes and Default Layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,19 @@
-export default function App() {}
+import { BrowserRouter, Routes, Route } from "react-router";
+//Layout
+import DefaultLayout from "./layouts/DefaultLayout";
+//Pages
+import Home from "./pages/Home";
+import TripPage from "./pages/TripPage";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<DefaultLayout />}>
+          <Route index path="/" element={<Home />} />
+          <Route path="/trips/:id" element={<TripPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
